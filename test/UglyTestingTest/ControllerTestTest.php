979f7417ae14ae06c86d01e20da84ba03b5e7042
Gary Hockin <gary@hock.in>
2014-12-20 10:21:05 +0000

    Renamed Test class to TestCase

--- test/UglyTestingTest/ControllerTestTest.php
+++ test/UglyTestingTest/ControllerTestTest.php
@@ -15,7 +15,7 @@ use Zend\View\Model\ViewModel;
 class AbstractControllerTestTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var SampleControllerTestFile
+     * @var SampleControllerTestCaseFile
      */
     protected $controllerTest;
 
